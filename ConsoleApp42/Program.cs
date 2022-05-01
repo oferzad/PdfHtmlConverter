0e7c591a3d2fe23f717692477498cfc489217906
Ofer Zadikario <ofer.zadikario@gmail.com>
2022-05-01 20:20:32 +0300

    clean up

--- ConsoleApp42/Program.cs
+++ ConsoleApp42/Program.cs
@@ -6,22 +6,7 @@ namespace ConsoleApp42
 {
     class Program
     {
-        static string ConvertHtml(string html)
-        {
-            //Manipulate html
-            int widthStartIndex = html.IndexOf("width=\"");
-            int heightStartIndex = html.IndexOf("height=\"");
-            int widthEndIndex = html.IndexOf("\"", widthStartIndex + 7);
-            int heightEndIndex = html.IndexOf("\"", heightStartIndex + 8);
-            string width = html.Substring(widthStartIndex + 7, widthEndIndex - widthStartIndex - 7);
-            string height = html.Substring(heightStartIndex + 8, heightEndIndex - heightStartIndex - 8);
-            int continueIndex = html.IndexOf(">", heightEndIndex);
-
-            string newHtml = html.Substring(0, widthStartIndex - 1);
-            newHtml += $" width=\"100%\" height=\"{height}\" viewBox=\"0 0 {width} {height}\" preserveaspectration=\"xMidYMid meet\"";
-            newHtml += html.Substring(continueIndex);
-            return newHtml;
-        }
+        
         static void Main(string[] args)
         {
 			//<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="main1" width="100%" height="1121"
